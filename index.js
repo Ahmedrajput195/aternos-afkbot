cb5eb2812e313695d59e60b73f4db7f6588590f3
Ahmedrajput195 <124665428+Ahmedrajput195@users.noreply.github.com>
2024-10-19 21:29:22 +0500

    index.js

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ let data = JSON.parse(rawdata);
 var lasttime = -1;
 var moving = 0;
 var connected = 0;
-var actions = [ 'forward', 'back', 'left', 'right']
+var actions = [ 'forward', 'back', 'left', 'right', 'jump']
 var lastaction;
 var pi = 3.14159;
 var moveinterval = 2; // 2 second movement interval
 var maxrandom = 5; // 0-5 seconds added to movement interval (randomly)
-var host = data["ip"];
-var port = data["port"];
-var username = data["name"]
+var host = data["Friends_Era.aternos.me"];
+var port = data["52726"];
+var username = data["AFK BOT"]
 var bot = mineflayer.createBot({
   host: host,
   port: port,
